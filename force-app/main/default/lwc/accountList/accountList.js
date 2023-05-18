8135b382cd1e44913105bdfade99e06827c5107c
Danny-JaJr <dlaf@qlead.io>
2023-05-18 11:31:17 -0400

    search added

--- force-app/main/default/lwc/accountList/accountList.js
+++ force-app/main/default/lwc/accountList/accountList.js
@@ -79,199 +79,52 @@ import getllAccountsSorting from '@salesforce/apex/AccountController.getllAccoun
 export default class AccountList extends LightningElement {
     //display accounts
      key;
-     @track accounts = [];
-     @track sortedBy= 'Name';
-     @track sortedDirection ='ASC';
-
-
-     cols = [
-        { label: 'Account Name', fieldName: 'Name', type: 'text', editable: true, sortable: 'true' },
-        { label: 'Industry', fieldName: 'Industry', type: 'text' , editable: true, sortable: 'true'  },
-        { label: 'Annual Revenue', fieldName: 'AnnualRevenue', type: 'currency' , editable: true, sortable: 'true' },
-        { label: 'Phone', fieldName: 'Phone', type: 'phone' , editable: true, sortable: 'true' },
-        { label: 'Website', fieldName: 'Website', type: 'url' , editable: true, sortable: 'true' }
-    ];
-
-     
-     //function that handle the search keywords
-     updateKey(event){
+     @track accounts;
+       //function that handle the search keywords
+       updateKey(event){
         this.key = event.target.value;
      }
 
-
-     // Function to handle search button click
+      //Function to handle search button click
     handleSearch() {
-        this.loadAccounts();
-    }
-
-    // Function to handle sorting
-    handleSort(event) {
-        const { fieldName, sortDirection } = event.detail;
-        this.sortedBy = fieldName;
-        this.sortedDirection = sortDirection;
-        this.loadAccounts();
-    }
-   
-     loadAccounts() {
-        this.accounts = undefined; // Clear previous data
-        this.error = undefined; // Clear previous error
-    }
-  // /getllAccountsSorting(String searchkey, String sortedBy, String sortedDirection)
-   // @wire(getllAccountsSorting, {searchkey: '$key', sortedBy: '$sortedBy', sortedDirection: '$sortedDirection'})accounts;
-     @wire(getAccounts, {searchkey: '$key'}) accounts;
-    //  @wire(getAccounts, {searchkey: '$key', direction:'$sortedDirection'})
-    //  wiredAccounts(result){
-    //    if(result.data){
-    //        this.accounts = result.data;
-    //        this.error = undefined;
-    //     }else if(result.error){
-    //         this.error = result.error;
-    //         this.data = undefined;
-    //         console.log(' The error is: ' + JSON.stringify(this.error));
-
-    //  }
-    // }
-
-    onSort(event){
-        this.sortedBy = event.detail.fieldName;
-        this.sortedDirection = event.detail.sortDirection
-        // this.sortData(this.sortedBy, this.sortedDirection);
-     }
-
-     sortData(fieldName, direction){
-        let parseData = JSON.parse(JSON>stringify(this.accounts));
-        let keyValue = (a) =>{
-            return a[fieldName];
-        };
-        let isReverse = direction === 'ASC' ? 1: -1;
-        parseData.sort((x, y) =>{
-            x = keyValue(x) ? keyValue(x): '';
-            y = keyValue(y) ? keyValue(y) : '';
-            return isReverse * ((x>y) - (y>x));
+        getAccounts({searchkey: this.key})
+        .then(result =>{
+            this.accounts = result;
         })
-        this.accounts = parseData;
-     }
+        .catch(error=>{
+            this.accounts = null;
+        });
+    }
+
+
+
+     cols = [
+        { label: 'Account Name', fieldName: 'Name', type: 'text' },
+        { label: 'Industry', fieldName: 'Industry', type: 'text' },
+        { label: 'Annual Revenue', fieldName: 'AnnualRevenue', type: 'currency'  },
+        { label: 'Phone', fieldName: 'Phone', type: 'phone'  },
+        { label: 'Website', fieldName: 'Website', type: 'url'  }
+    ];
+
+     
+
+
+    
+  
+   
+ 
+
+    // @wire(getAccounts, {searchkey: '$key'}) accounts;
+
+
+
+
+
 }
 
 
-   //@wire(getAccounts, {searchkey: '$key',sortedBy: '$sortedBy', sortedDirection: '$sortedDirection' }) accounts;
-//    @wire(getAccounts, {searchkey: '$key'}) accounts;
-    // handleSearch(){
-    //     //call the Apex method
-    //     getAccounts({searchkey: this.key, sortedBy: this.sortedBy, sortedDirection: this.sortedDirection})
-    //     .then(result =>{
-    //         this.accounts = result
-    //         // this.sortedBy !== 'asc'
-    //     })
-    //     .catch(error=>{
-    //         this.accounts = null;
-    //     });
-    // }
-     
-//   sortData(fieldName, sortDirection){
-//     var data = this.accounts;
-//     var keys = a => a[fieldName];
-//     var reverse = sortDirection === 'asc' ? l: -l;
-//     data.sort(a,b) => {
-//         let valueA = keys(a) ? keys(a).toLowercase() : '';
-//         let valueB = keys(b) ? keys(b).toLowercase() : '';
-//         return reverse * ((valueA > valueB) -(valueB > valueA));
-//     });
-//     this.accounts = data
- // }
 
 
 
-    // handleColumnSorting(event) {
-    //     const { fieldName, sortDirection } = event.detail;
-    //     this.sortedBy = fieldName;
-    //     this.sortedDirection = sortDirection;
-    // }
-
-
-
-// const columns = [
-//     { label: 'Account Name', fieldName: 'Name', type: 'text' },
-//     { label: 'Industry', fieldName: 'Industry', type: 'text' },
-//     { label: 'Annual Revenue', fieldName: 'AnnualRevenue', type: 'currency' },
-//     { label: 'Phone', fieldName: 'Phone', type: 'phone' },
-//     { label: 'Website', fieldName: 'Website', type: 'url' }
-// ];
-
-   // accountName= '';
-    // @track accountList = [];
-    // columns = columns;
-
-    // @wire(getAccounts, {actName:'$accountName'})
-    // retrieveAccounts({error, data}){
-    //     if(data){
-    //         this.accountList = data;
-    //     }
-    //     else if(error){
-    //         console.error('Error retrieving accounts:', error);
-
-    //     }
-    // }
-    
-    // handleKeyChange(event){
-    //     this.accountName = event.target.value;
-    // }
-
-
-
-
- //  function for the button thaat call the Apex
-    // handleSearch(){
-    //     //call the Apex method
-    //     getAccounts({searchkey: this.key})
-    //     .then(result =>{
-    //         this.accounts = result
-    //     })
-    //     .catch(error=>{
-    //         this.accounts = null;
-    //     });
-    // }
-
-
-//   accounts = [];
-//   columns = [
-//     { label: 'Account Name', fieldName: 'Name', type: 'text' },
-//     { label: 'Industry', fieldName: 'Industry', type: 'text' },
-//     { label: 'Annual Revenue', fieldName: 'AnnualRevenue', type: 'currency' },
-//     { label: 'Phone', fieldName: 'Phone', type: 'phone' },
-//     { label: 'Website', fieldName: 'Website', type: 'url' }
-//   ];
-
-//   @wire(getAccounts)
-//   wiredAccounts({ error, data }) {
-//     if (data) {
-//       this.accounts = data;
-//     } else if (error) {
-//       // Handle error and display appropriate message
-//       this.handleError(error)
-//     }
-//   }
-
-//   handleSearch(event) {
-//     const searchTerm = event.target.value.toLowerCase();
-//     if (searchTerm) {
-//       const filteredAccounts = this.accounts.filter(
-//         (account) =>
-//           account.Name.toLowerCase().includes(searchTerm) ||
-//           account.Industry.toLowerCase().includes(searchTerm) ||
-//           account.Phone.toLowerCase().includes(searchTerm) ||
-//           account.Website.toLowerCase().includes(searchTerm)
-//       );
-//       this.accounts = filteredAccounts;
-//     } else {
-//       // Reset the account list if the search term is empty
-//       this.accounts = [...this.accounts];
-//     }
-//   }
-
-//   handleError(error) {
-//     // Handle the error and display appropriate message
-//     console.error('An error occurred while retrieving accounts:', error);
-//     // Display an error toast or show an error message on the component
-//   }
-
+ 
+ 
--- force-app/main/default/lwc/customSearch/customSearch.js
+++ force-app/main/default/lwc/customSearch/customSearch.js
@@ -1,72 +1,108 @@
 import { LightningElement, wire, track } from 'lwc';
 import getAccounts from '@salesforce/apex/ListControllerLwc.getAccounts';
-import ACCOUNT_NAME_FIELD from '@salesforce/schema/Account.Name';
-//import ACCOUNT_TYPE_FIELD from '@salesforce/schema/Account.Type';
-import ACCOUNT_PHONE_FIELD from '@salesforce/schema/Account.Phone';
-import ACCOUNT_INDUSTRY from '@salesforce/schema/Account.Industry';
-import ACCOUNT_REVENUE from '@salesforce/schema/Account.AnnualRevenue';
-import ACCOUNT_WEBSITE from '@salesforce/schema/Account.Website';
-
-// const COLUMNS = [
-//     { label: 'Account Name', fieldName: 'Name', type: 'text', editable: true, sortable: 'true' },
-//     { label: 'Industry', fieldName: 'Industry', type: 'text' , editable: true, sortable: 'true'  },
-//     { label: 'Annual Revenue', fieldName: 'AnnualRevenue', type: 'currency' , editable: true, sortable: 'true' },
-//     { label: 'Phone', fieldName: 'Phone', type: 'phone' , editable: true, sortable: 'true' },
-//     { label: 'Website', fieldName: 'Website', type: 'url' , editable: true, sortable: 'true' }
-// ];
+// import ACCOUNT_NAME_FIELD from '@salesforce/schema/Account.Name';
+// //import ACCOUNT_TYPE_FIELD from '@salesforce/schema/Account.Type';
+// import ACCOUNT_PHONE_FIELD from '@salesforce/schema/Account.Phone';
+// import ACCOUNT_INDUSTRY from '@salesforce/schema/Account.Industry';
+// import ACCOUNT_REVENUE from '@salesforce/schema/Account.AnnualRevenue';
+// import ACCOUNT_WEBSITE from '@salesforce/schema/Account.Website';
 
 const COLUMNS = [
-    {
-        label: 'Account Name',
-        fieldName: ACCOUNT_NAME_FIELD.fieldApiName,
-        type: 'text',
-        editable: 'true',
-        editable: true, 
-        sortable: 'true'
-    },
-    { 
-        label: 'Industry', 
-        fieldName: ACCOUNT_INDUSTRY, 
-        type: 'text' , 
-        editable: true, 
-        sortable: 'true'  
-    },
-    {
-        label: 'Phone',
-        fieldName: ACCOUNT_PHONE_FIELD.fieldApiName,
-        type: 'phone',
-        editable: 'true',
-        editable: true, 
-        sortable: 'true'
-    },    
-    { 
-        label: 'Website', 
-        fieldName: ACCOUNT_WEBSITE.fieldApiName,
-        type: 'url' , 
-        editable: true, 
-        sortable: 'true' 
-    },
-    { 
-        label: 'Annual Revenue', 
-        fieldName: ACCOUNT_REVENUE.fieldApiName,
-         type: 'currency' ,
-        editable: true, 
-        sortable: 'true' 
-    },
+    { label: 'Account Name', fieldName: 'Name', type: 'text', editable: true, sortable: 'true' },
+    { label: 'Industry', fieldName: 'Industry', type: 'text' , editable: true, sortable: 'true'  },
+    { label: 'Annual Revenue', fieldName: 'AnnualRevenue', type: 'currency' , editable: true, sortable: 'true' },
+    { label: 'Phone', fieldName: 'Phone', type: 'phone' , editable: true, sortable: 'true' },
+    { label: 'Website', fieldName: 'Website', type: 'url' , editable: true, sortable: 'true' }
+];
+
+// const COLUMNS = [
+//     {
+//         label: 'Account Name',
+//         fieldName: ACCOUNT_NAME_FIELD.fieldApiName,
+//         type: 'text',
+//         sortable: 'true'
+//     },
+//     { 
+//         label: 'Industry', 
+//         fieldName: ACCOUNT_INDUSTRY, 
+//         type: 'text' , 
+//         sortable: 'true'  
+//     },
+//     {
+//         label: 'Phone',
+//         fieldName: ACCOUNT_PHONE_FIELD.fieldApiName,
+//         type: 'phone',
+//         sortable: 'true'
+//     },    
+//     { 
+//         label: 'Website', 
+//         fieldName: ACCOUNT_WEBSITE.fieldApiName,
+//         type: 'url' , 
+//         sortable: 'true' 
+//     },
+//     { 
+//         label: 'Annual Revenue', 
+//         fieldName: ACCOUNT_REVENUE.fieldApiName,
+//          type: 'currency' ,
+//         sortable: 'true' 
+//     },
   
 
-];
+// ];
 
 export default class CustomSearch extends LightningElement {
     key;
     @track accounts = [];
 
-    columns = COLUMNS;
-
+    @track data;
+    @track columns = COLUMNS;
+    // @wire(getAccounts)
+    // accounts;
 
     @wire(getAccounts)
-    accounts;
+    accouns(result) {
+        if (result.data) {
+            this.data = result.data;
+            this.error = undefined;
+        } else if (result.error) {
+            this.error = result.error;
+            this.data = undefined;
+        }
+    }
 
-  
+    @track data;
+    @track sortBy;
+    @track sortDirection;
+
+    updateKey(event){
+        this.key = event.target.value;
+     }
+    
+
+    
+    doSorting(event) {
+        this.sortBy = event.detail.fieldName;
+        this.sortDirection = event.detail.sortDirection;
+        this.sortData(this.sortBy, this.sortDirection);
+    }
+
+    sortData(fieldname, direction) {
+        let parseData = JSON.parse(JSON.stringify(this.data));
+        // Return the value stored in the field
+        let keyValue = (a) => {
+            return a[fieldname];
+        };
+        // cheking reverse direction
+        let isReverse = direction === 'asc' ? 1: -1;
+        // sorting data
+        parseData.sort((x, y) => {
+            x = keyValue(x) ? keyValue(x) : ''; // handling null values
+            y = keyValue(y) ? keyValue(y) : '';
+            // sorting values based on direction
+            return isReverse * ((x > y) - (y > x));
+        });
+        this.data = parseData;
+    }
+
    
 }
