138537a2a428efe4df7b4c32f57e743f44817833
Danny-JaJr <dlaf@qlead.io>
2023-05-18 13:40:36 -0400

    pagination added

--- force-app/main/default/lwc/accountList/accountList.js
+++ force-app/main/default/lwc/accountList/accountList.js
@@ -1,35 +1,74 @@
 import { LightningElement, wire, track } from 'lwc';
 import getAccounts from '@salesforce/apex/AccountController.getAccounts';
 
-const columns = [
-    { label: 'Account Name', fieldName: 'Name', type: 'text', editable: true, sortable: 'true' },
-    { label: 'Industry', fieldName: 'Industry', type: 'text' , editable: true, sortable: 'true'  },
-    { label: 'Annual Revenue', fieldName: 'AnnualRevenue', type: 'currency' , editable: true, sortable: 'true' },
-    { label: 'Phone', fieldName: 'Phone', type: 'phone' , editable: true, sortable: 'true' },
-    { label: 'Website', fieldName: 'Website', type: 'url' , editable: true, sortable: 'true' }
-];
+
 
 export default class AccountList extends LightningElement {
     
     @track data;
     @track sortBy;
     @track sortDirection;
-    @track columns = columns;
+    @track columns;
      key;
+
+       // JS Properties 
+    pageSizeOptions = [3, 5, 7]; //Page size options
+    records = []; //All records available in the data table
+    columns = []; //columns information available in the data table
+    totalRecords = 0; //Total no.of records
+    pageSize; //No.of records to be displayed per page
+    totalPages; //Total no.of pages
+    pageNumber = 1; //Page number    
+    //recordsToDisplay = []; //Records to be displayed on the page = data
+    
+
+
     // @track accounts;
        //function that handle the search keywords
        updateKey(event){
         this.key = event.target.value;
      }
 
+
+     get bDisableFirst() {
+        return this.pageNumber == 1;
+    }
+
+    get bDisableLast() {
+        return this.pageNumber == this.totalPages;
+    }
+
+
+     // connectedCallback method called when the element is inserted into a document
+     connectedCallback() {
+        // set datatable columns info
+        this.columns = [
+            { label: 'Account Name', fieldName: 'Name', type: 'text', editable: true, sortable: 'true' },
+            { label: 'Industry', fieldName: 'Industry', type: 'text' , editable: true, sortable: 'true'  },
+            { label: 'Annual Revenue', fieldName: 'AnnualRevenue', type: 'currency' , editable: true, sortable: 'true' },
+            { label: 'Phone', fieldName: 'Phone', type: 'phone' , editable: true, sortable: 'true' },
+            { label: 'Website', fieldName: 'Website', type: 'url' , editable: true, sortable: 'true' }
+        ];
+
+    
+     
+    }
+
+
       //Function to handle search button click
     handleSearch() {
         getAccounts({searchkey: this.key})
         .then(result =>{
             this.data = result;
+            console.log('RESULT--> ' + JSON.stringify(result));
+                    this.records = result;
+                    this.totalRecords = result.length; // update total records count                 
+                    this.pageSize = this.pageSizeOptions[0]; //set pageSize with default value as first option
+                    this.paginationHelper(); // call helper menthod to update pagination logic 
         })
         .catch(error=>{
             this.data = null;
+            console.log('error while fetch contacts--> ' + JSON.stringify(error));
         });
     }
 
@@ -69,6 +108,58 @@ export default class AccountList extends LightningElement {
             return isReverse * ((x > y) - (y > x));
         });
         this.data = parseData;
+    }
+
+
+
+
+
+    
+    handleRecordsPerPage(event) {
+        this.pageSize = event.target.value;
+        this.paginationHelper();
+    }
+
+    previousPage() {
+        this.pageNumber = this.pageNumber - 1;
+        this.paginationHelper();
+    }
+
+    nextPage() {
+        this.pageNumber = this.pageNumber + 1;
+        this.paginationHelper();
+    }
+
+    firstPage() {
+        this.pageNumber = 1;
+        this.paginationHelper();
+    }
+
+    lastPage() {
+        this.pageNumber = this.totalPages;
+        this.paginationHelper();
+    }
+
+
+    // JS function to handel pagination logic 
+    paginationHelper() {
+        this.data = [];
+        // calculate total pages
+        this.totalPages = Math.ceil(this.totalRecords / this.pageSize);
+        // set page number 
+        if (this.pageNumber <= 1) {
+            this.pageNumber = 1;
+        } else if (this.pageNumber >= this.totalPages) {
+            this.pageNumber = this.totalPages;
+        }
+
+        // set records to display on current page 
+        for (let i = (this.pageNumber - 1) * this.pageSize; i < this.pageNumber * this.pageSize; i++) {
+            if (i === this.totalRecords) {
+                break;
+            }
+            this.data.push(this.records[i]);
+        }
     }
 
 }
